634fc13e6aea1bb9d7f2eea27c48229509a57aa9
Steven Gouws <steven@stevengouws.com>
2024-01-16 14:22:14 +0000

    Setup

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { describe, it, beforeEach, afterEach } from "mocha";
+
+describe("it works", () => {
+  it("does work", async () => {
+    expect(false).not.to.be.true;
+  });
+});
